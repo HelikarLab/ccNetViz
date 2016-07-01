b909eb4e77f8c24bab2da6ae428fefb103fa06bc
Aleš Saska <mail@alessaska.cz>
2016-07-01 14:03:05 +0200

    first tests of spatial search

---
+++ tests/js/spatialsearch_complex.js
@@ -0,0 +1,5 @@
+$(function(){
+  QUnit.test( "Spatial search - complex", function( assert ) {
+      //TODO: add some more complex tests after "predefined" layout instead of "force" would be implemented
+  });
+});
