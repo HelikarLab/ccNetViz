ee783573317dbd7743c11d65f179b5bf2052be75
HelikarLab <david.tichy@post.cz>
2016-08-04 21:07:50 -0500

    layout.normalize: input output dimension

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -12,29 +12,44 @@ layout.force = layoutForce;
 layout.random = layoutRandom;
 
 
-layout.normalize = function(nodes) {
-    var n = nodes.length;
-    var maxX = -Infinity;
-    var maxY = -Infinity;
-    var minX = Infinity;
-    var minY = Infinity;
+layout.normalize = function(nodes, dim) {
+    var minX, minY, n = nodes.length;
+    
+    if (dim) {
+        minX = dim.minX;
+        minY = dim.minY;
+    }
+    else {
+        var maxX = -Infinity;
+        var maxY = -Infinity;
+        minX = minY = Infinity;
+        
+        for (var i = 0; i < n; i++) {
+            var o = nodes[i];
+            maxX = Math.max(maxX, o.x);
+            maxY = Math.max(maxY, o.y);
+            minX = Math.min(minX, o.x);
+            minY = Math.min(minY, o.y);
+        };
+        
+        dim = {
+            maxX: maxX,
+            maxY: maxY,
+            minX: minX,
+            minY: minY
+        }
+    }
 
-    for (var i = 0; i < n; i++) {
-        var o = nodes[i];
-        maxX = Math.max(maxX, o.x);
-        maxY = Math.max(maxY, o.y);
-        minX = Math.min(minX, o.x);
-        minY = Math.min(minY, o.y);
-    };
-
-    var scX = minX !== maxX ? 1 / (maxX - minX) : ((minX -= 0.5), 1);
-    var scY = minY !== maxY ? 1 / (maxY - minY) : ((minY -= 0.5), 1);
+    var scX = minX !== dim.maxX ? 1 / (dim.maxX - minX) : ((minX -= 0.5), 1);
+    var scY = minY !== dim.maxY ? 1 / (dim.maxY - minY) : ((minY -= 0.5), 1);
 
     for (var i = 0; i < n; i++) {
         var o = nodes[i];
         o.x = scX * (o.x - minX);
         o.y = scY * (o.y - minY);
     }
+    
+    return dim;
 }
 
 module.exports = layout;
