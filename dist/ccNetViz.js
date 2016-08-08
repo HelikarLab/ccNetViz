4fc305597716c96d0564ae1e97bc26199469c3a6
Aleš Saska <mail@alessaska.cz>
2016-08-08 21:56:37 +0200

    builded version

--- dist/ccNetViz.js
+++ dist/ccNetViz.js
@@ -596,15 +596,15 @@
 	    canvas.height = canvas.offsetHeight;
 	  }
 
-	  this.setViewport = function(v) {
+	  this.setViewport = function(v, silent) {
 	    if(checkRemoved()) return;
 
 	    for(var k in v){view[k] = v[k]};
 
-	    options.onChangeViewport(view);
+	    !silent && options.onChangeViewport(view);
 	  }
 
-	  this.resetView = () => this.setViewport({size:1,x:0,y:0});
+	  this.resetView = (silent) => this.setViewport({size:1,x:0,y:0}, silent);
 
 	  var self = this;
 	  //expose these methods from layer into this class
@@ -630,7 +630,7 @@
 	  view = {};
 	  var context = {};
 
-	  this.resetView();
+	  this.resetView(true);
 	  this.resize();
 
 	  var textures = new ccNetViz_textures(options.onLoad || this.draw);
