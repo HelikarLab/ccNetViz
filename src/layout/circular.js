b7600d43d35047c48722a31c48e5989345759772
Renato Fabbri <renato.fabbri@gmail.com>
2017-06-15 05:29:08 -0300

    tabs problem

--- src/layout/circular.js
+++ src/layout/circular.js
@@ -23,30 +23,30 @@ export default class {
     angle = 2.Math.PI/nodes.length;
   }
   function degrees(nodes, edges) {
-			// should return ordered nodes and their degrees - high to low
-			var degrees = Array(nodes.length).fill(0);
-			edges.forEach(function(e) {
-					degrees[e.source.index] += 1;
-					degrees[e.target.index] += 1;
-			}); // check to see if not getting double of the degree in undirected graphs
-			//getting the order of nodes from highest to lowest degrees
-			var mapped = degrees.map(function(el, i) {
-					return { index: i, value: el };
-			});
-			mapped.sort(function(a, b) {
-					return +(a.value < b.value) || +(a.value === b.value) - 1;
-			});
-			ordered_degrees = mapped.map(function(el){
-					return degrees[el.index];
-			});
-			return mapped_nodes, ordered_degrees;
+      // should return ordered nodes and their degrees - high to low
+      var degrees = Array(nodes.length).fill(0);
+      edges.forEach(function(e) {
+          degrees[e.source.index] += 1;
+          degrees[e.target.index] += 1;
+      }); // check to see if not getting double of the degree in undirected graphs
+      //getting the order of nodes from highest to lowest degrees
+      var mapped = degrees.map(function(el, i) {
+          return { index: i, value: el };
+      });
+      mapped.sort(function(a, b) {
+          return +(a.value < b.value) || +(a.value === b.value) - 1;
+      });
+      ordered_degrees = mapped.map(function(el){
+          return degrees[el.index];
+      });
+      return mapped_nodes, ordered_degrees;
   }
   apply () {
-			var nodes_, degrees_ = degrees(this._nodes, this._edges);
-			nodes_.forEach(function(node, i){
-					node.x = Math.cos(i*angle);
-					node.y = Math.sin(i*angle);
-					node.weight = degrees_[i];
-			}); 
+      var nodes_, degrees_ = degrees(this._nodes, this._edges);
+      nodes_.forEach(function(node, i){
+          node.x = Math.cos(i*angle);
+          node.y = Math.sin(i*angle);
+          node.weight = degrees_[i];
+      }); 
   }
 };
