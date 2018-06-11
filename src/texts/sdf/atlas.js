f2a377536f80d63395d6ea0fd078a572bd22762f
Gaurav Grover <gauravgrover95@gmail.com>
2018-06-12 02:35:12 +0530

    adds some documentation to atlas.js

--- src/texts/sdf/atlas.js
+++ src/texts/sdf/atlas.js
@@ -2,43 +2,102 @@
 
 import ShelfPack from 'shelf-pack';
 
-const SIZE_GROWTH_RATE = 4;
+// size of the spritesheet generated in pixels
 const DEFAULT_SIZE = 1024;
+
+// multiplication factor by which spritesheet can grow, if shelfpack is full
+const SIZE_GROWTH_RATE = 4;
+
+// max size to contain all the characters in the spritesheet
 // must be "DEFAULT_SIZE * SIZE_GROWTH_RATE ^ n" for some integer n
 const MAX_SIZE = 2048;
 
 export default class GlyphAtlas {
-
     constructor(gl, resetCache) {
+
+        // setting width to default
         this.width = DEFAULT_SIZE;
+
+        // setting height to default
         this.height = DEFAULT_SIZE;
 
+        // resets cached glyphs in sdf.js before making resize() call
         this._resetCache = resetCache;
+
+        // creating an empty spritesheet to pack glyphs
         this.bin = new ShelfPack(this.width, this.height);
+
+        /**
+         * stores information coordinate about character
+         * 
+         * (Object):
+         *  "fontstring#charid": {
+         *      id, x, y, h, w, maxh, maxw, ref_count
+         *  }
+         */
         this.index = {};
+
+        /**
+         * stores list of character ids available in atlas.js
+         * 
+         * (Object):
+         *  "fontstring#charid": ["charid"]
+         */
         this.ids = {};
-      
+
+        // webgl rendering context
         this.gl = gl;
+
+        // initialized to empty Uint8Array buffer to store texture data
         this.data = new Uint8Array(this.width * this.height);
-        
-        
     }
-    
-    _createTexture(){
+
+
+    // refreshes texture of characters when updateTexture() is called
+    _createTexture() {
+        // 
         this.dirty = false;
+
+        // standard creation of webgl texture
         let gl = this.gl;
         let texture = gl.createTexture();
+        // binds texture buffer to the target (gl.TEXTURE_2D = a 2 dimensional texture)
         gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        /**
+         * pixelStorei() (Func): specifies the pixel storage modes
+         * UNPACK_FLIP_Y_WEBGL (Parameter): Flips the source data along its vertical axis if true.
+         */
         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+
+        // standard interpolation Filters
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.ALPHA, this.width, this.height, 0, gl.ALPHA, gl.UNSIGNED_BYTE, this.data);
+
+        // specifies a two-dimensional image for texture buffer stored in variable named "texture"
+        gl.texImage2D(
+            gl.TEXTURE_2D, // target: A two-dimensional texture
+            0, // level of detail: 0 = base image level; n = nth mipmap reduction level
+            gl.ALPHA, // internalFormat: only reads the alpha component
+            this.width, // width
+            this.height, // height 
+            0, // border
+            gl.ALPHA, // format
+            gl.UNSIGNED_BYTE, // type
+            this.data // ImageData object
+        );
+
+
+        // Unbinding the buffer channel
         gl.bindTexture(gl.TEXTURE_2D, null);
+
+        // returning so formed texture
         return texture;
     }
 
+    // utility function that returns list of glyphs added
     getGlyphs() {
         const glyphs = {};
         let split,
@@ -53,10 +112,10 @@ export default class GlyphAtlas {
             if (!glyphs[name]) glyphs[name] = [];
             glyphs[name].push(id);
         }
-
         return glyphs;
     }
 
+    // utility function that returns list of rects added
     getRects() {
         const rects = {};
         let split,
@@ -71,16 +130,23 @@ export default class GlyphAtlas {
             if (!rects[name]) rects[name] = {};
             rects[name][id] = this.index[key];
         }
-
         return rects;
     }
 
-    addGlyph(id, name, glyph, buffer, markDirty) {
+    // main function of the module called from sdf.js
+    addGlyph(
+        id, // character id
+        name, // name of the font - "fontstrong#range.pbf"
+        glyph, // glyph object from sdf.js
+        buffer, // padding around glyph
+        markDirty // 
+    ) {
         if (!glyph) return null;
 
+        // compiles 'name' and glyph.id into a single string with a seperator '#'
         const key = `${name}#${glyph.id}`;
 
-        // The glyph is already in this texture.
+        // if key is present in index and not present in ids then add it in ids
         if (this.index[key]) {
             if (this.ids[key].indexOf(id) < 0) {
                 this.ids[key].push(id);
@@ -92,46 +158,54 @@ export default class GlyphAtlas {
         const bufferedHeight = glyph.height + buffer * 2;
 
         // Add a 1px border around every image.
+        // 'padding' variable is basically border
+        // buffer variable is basically padding
         const padding = 1;
         let packWidth = bufferedWidth + 2 * padding;
         let packHeight = bufferedHeight + 2 * padding;
 
         // Increase to next number divisible by 4, but at least 1.
-        // This is so we can scale down the texture coordinates and pack them
-        // into fewer bytes.
+        // This is so we can scale down the texture coordinates and pack them into fewer bytes
         packWidth += (4 - packWidth % 4);
         packHeight += (4 - packHeight % 4);
 
         let rect = this.bin.packOne(packWidth, packHeight);
+
+        // if the current size is not sufficient to contain all the characters in the texture
+        // then, expand (resize) it
         if (!rect) {
             this.resize();
             rect = this.bin.packOne(packWidth, packHeight);
+            // if markDirty callback was passed then execute it
             markDirty && markDirty();
         }
+
+        // if still not found then there is some problem, simply return null to indicate problem
         if (!rect) {
             return null;
         }
 
+
         this.index[key] = rect;
         this.ids[key] = [id];
 
+        // if bitmap corresponding to the glyph object exists then, add the glyph 
         if (glyph.bitmap) {
-          const target = this.data;
-          const source = glyph.bitmap;
-          for (let y = 0; y < bufferedHeight; y++) {
-              const y1 = this.width * (rect.y + y + padding) + rect.x + padding;
-              const y2 = bufferedWidth * y;
-              for (let x = 0; x < bufferedWidth; x++) {
-                  target[y1 + x] = source[y2 + x];
-              }
-          }
+            const target = this.data;
+            const source = glyph.bitmap;
+            for (let y = 0; y < bufferedHeight; y++) {
+                const y1 = this.width * (rect.y + y + padding) + rect.x + padding;
+                const y2 = bufferedWidth * y;
+                for (let x = 0; x < bufferedWidth; x++) {
+                    target[y1 + x] = source[y2 + x];
+                }
+            }
         }
-          
         this.dirty = true;
-
         return rect;
     }
 
+    // expands the size of the texture if all the characters were not contained in default size
     resize() {
         const prevWidth = this.width;
         const prevHeight = this.height;
@@ -161,8 +235,9 @@ export default class GlyphAtlas {
 
     bind(gl) {
     }
-    
-    get texture(){
+
+    // getter of texture from GlyphAtlas object
+    get texture() {
         return this._texture;
     }
 
@@ -171,10 +246,26 @@ export default class GlyphAtlas {
         if (!this._texture) {
             this._texture = this._createTexture();
         }
-        if(this.dirty){
+
+        // if dirty functionality is true then: 
+        if (this.dirty) {
+
             gl.bindTexture(gl.TEXTURE_2D, this._texture);
+
             gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
-            gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, this.width, this.height, gl.ALPHA, gl.UNSIGNED_BYTE, this.data);
+
+            // specifies a sub - rectangle of the current texture
+            gl.texSubImage2D(
+                gl.TEXTURE_2D,
+                0,
+                0,
+                0,
+                this.width,
+                this.height,
+                gl.ALPHA,
+                gl.UNSIGNED_BYTE,
+                this.data
+            );
             gl.bindTexture(gl.TEXTURE_2D, null);
             this.dirty = false;
         }
