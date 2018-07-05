1cd012004e7b887b819194f968bc211bfc4a6c07
Gaurav Grover <gauravgrover95@gmail.com>
2018-07-05 19:44:30 +0530

    Changes glyph advancement 11 -> this.size

--- dist/ccNetViz.js
+++ dist/ccNetViz.js
@@ -12876,7 +12876,7 @@
 	                top: 0,
 	                width: this.size,
 	                height: this.size,
-	                advance: 11
+	                advance: this.size
 	            };
 	        }
 	
