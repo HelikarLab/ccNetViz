d4fd4b839d5ee80c6cdc38afc11f5cc8f85cfe68
Gaurav Grover <gauravgrover95@gmail.com>
2018-07-28 03:37:49 +0530

    Fixes bugs and errors and successfully updates webpack to ^4.16.2

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
 /**
  *  Collecting Terminal Arguments
  */
-
 var flags = {};
 
 process.argv.forEach(function (val, index, array) {
@@ -38,83 +37,22 @@ function runClosueCompiler(){
         console.error(stdErr);
       }
     });
+    console.log(' --- Closure compiler executed successfully');  
 }
 
-/**
- * Webpack Build Scrpit
- */
-var Webpack = require("webpack");
-var path = require('path');
 
-// returns a Compiler instance
-var webpackinst = Webpack({
-  
-  entry: './src/ccNetVizMultiLevel.js',
-  
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'ccNetViz.js',
-  },
+setTimeout(function () {
+  console.log(' --- appending module.exports into dist/ccNetViz.js (import fix)');
+  var fs = require('fs');
+  var filedata = [
+    fs.readFileSync(__dirname + '/dist/ccNetViz.js').toString(),
+    'if(typeof module !== "undefined")',
+    'module.exports = ccNetViz;'
+  ];
 
+  //append to file export
+  fs.writeFileSync(__dirname + '/dist/ccNetViz.js', filedata.join('\n'));
 
-  /**
-   *   | devtool value  | build   | rebuild | production  | quality
-   *   | 'source-map'   | slow    | slow    | yes         | original source
-   */
-  devtool: 'source-map',
-
-  module:{
-    rules: [
-      // Loader 1
-      {
-        // Target Files
-        // test: /\.js?$/g,
-        
-        // Excluded folders
-        exclude: /(node_modules|bower_components)/,
-        
-        // The Loader
-        loader: 'babel',
-
-        // Loader Configurations
-        query: {
-          presets: ['es2015']
-        },
-      }
-    ]
-  },
-  plugins: [
-    new Webpack.LoaderOptionsPlugin({
-      debug: true
-    })
-  ],
-});
-
-
-console.log('Packing ccNetViz library with webpack');
-webpackinst.run(function(err, stats) {
-    if(err){
-        console.error(" --- ERROR in packing");
-        console.error(err);
-        return;
-    }
-
-
-    setTimeout(function(){
-      console.log(' --- appending module.exports into dist/ccNetViz.js (import fix)');
-      var fs = require('fs');
-      var filedata = [
-        fs.readFileSync(__dirname+'/dist/ccNetViz.js').toString(),
-        'if(typeof module !== "undefined")',
-        'module.exports = ccNetViz;'
-      ];
-
-      //append to file export
-      fs.writeFileSync(__dirname+'/dist/ccNetViz.js', filedata.join('\n'));
-
-      console.log(' --- Successfully packed into dist/ccNetViz.js');
-
-      console.log(' --- Executing closure compiler');
-      runClosueCompiler();
-    },5*100);
-});
+  console.log(' --- Executing closure compiler');
+  runClosueCompiler();
+}, 5 * 100);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var Webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+
+    entry: './src/ccNetVizMultiLevel.js',
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'ccNetViz.js',
+    },
+
+
+    /**
+     *   | devtool value  | build   | rebuild | production  | quality
+     *   | 'source-map'   | slow    | slow    | yes         | original source
+     */
+    devtool: 'source-map',
+
+    module: {
+        rules: [
+            // Loader 1
+            {
+                // Target Files
+                // test: /\.js?$/g,
+
+                // Excluded folders
+                exclude: /(node_modules|bower_components)/,
+
+                // The Loader
+                loader: 'babel-loader',
+
+                // Loader Configurations
+                query: {
+                    presets: ['es2015']
+                },
+            }
+        ]
+    },
+    plugins: [
+        new Webpack.LoaderOptionsPlugin({
+            debug: true
+        })
+    ],
+};
