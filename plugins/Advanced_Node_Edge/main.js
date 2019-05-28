33d06032bbc6817c180c8b960814d78f4b6358b4
Caner Akdas <canerakdas@gmail.com>
2019-05-28 12:49:30 +0300

    set method added

--- plugins/Advanced_Node_Edge/main.js
+++ plugins/Advanced_Node_Edge/main.js
@@ -5,43 +5,110 @@ export default class Plugin {
     this.canvas = document.createElement('canvas');
     this.canvas.width = 0;
     this.canvas.height = 0;
-    //this.canvas.style.display = 'none';
+    this.canvas.style.display = 'none';
   }
 
-  circle(config) {
-    const circle = new Circle(this.canvas, config);
-  }
+  set(config) {
+    this.config = config;
+    this.temp = {};
+    for (let key in this.config) {
+      switch (Object.keys(this.config[key])[0]) {
+        case "circle":
+          let circle = new Circle(this.canvas, this.config[key].circle);
 
-  ellipse(config) {
-    const ellipse = new Ellipse(this.canvas, config);
-  }
+          async function circleBlob(temp) {
+            let blob = await circle.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
 
-  triangle(config) {
-    const triangle = new Triangle(this.canvas, config);
-  }
+          circleBlob(this.temp);
+          break;
+        case "ellipse":
+          let ellipse = new Ellipse(this.canvas, this.config[key].ellipse);
 
-  rectangle(config) {
-    const rectangle = new Rectangle(this.canvas, config);
-  }
+          async function ellipseBlob(temp) {
+            let blob = await ellipse.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
 
-  rhombus(config) {
-    const rhombus = new Rhombus(this.canvas, config);
-  }
+          ellipseBlob(this.temp);
+          break;
+        case "triangle":
+          let triangle = new Triangle(this.canvas, this.config[key].triangle);
 
-  pentagon(config) {
-    const pentagon = new Pentagon(this.canvas, config);
-  }
+          async function triangleBlob(temp) {
+            let blob = await triangle.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
 
-  hexagon(config) {
-    const hexagon = new Hexagon(this.canvas, config);
-  }
+          triangleBlob(this.temp);
+          break;
+        case "rectangle":
+          let rectangle = new Rectangle(this.canvas, this.config[key].rectangle);
 
-  heptagon(config) {
-    const heptagon = new Heptagon(this.canvas, config);
-  }
+          async function rectangleBlob(temp) {
+            let blob = await rectangle.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
 
-  star(config) {
-    const star = new Star(this.canvas, config);
+          rectangleBlob(this.temp);
+          break;
+        case "rhombus":
+          let rhombus = new Rhombus(this.canvas, this.config[key].rhombus);
+
+          async function rhombusBlob(temp) {
+            let blob = await rhombus.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
+
+          rhombusBlob(this.temp);
+          break;
+        case "pentagon":
+          let pentagon = new Pentagon(this.canvas, this.config[key].pentagon);
+
+          async function pentagonBlob(temp) {
+            let blob = await pentagon.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
+
+          pentagonBlob(this.temp);
+          break;
+        case "hexagon":
+          let hexagon = new Hexagon(this.canvas, this.config[key].hexagon);
+
+          async function hexagonBlob(temp) {
+            let blob = await hexagon.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
+
+          hexagonBlob(this.temp);
+          break;
+        case "heptagon":
+          let heptagon = new Heptagon(this.canvas, this.config[key].heptagon);
+
+          async function heptagonBlob(temp) {
+            let blob = await heptagon.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
+
+          heptagonBlob(this.temp);
+          break;
+        case "star":
+          let star = new Star(this.canvas, this.config[key].star);
+
+          async function starBlob(temp) {
+            let blob = await star.toBlob;
+            temp[key] = { "texture": URL.createObjectURL(blob) };
+          }
+
+          starBlob(this.temp);
+          break;
+        default:
+          // TODO : Throw error and shapes link
+          break;
+      }
+    }
+    return this.temp
   }
 }
 
