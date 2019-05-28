c08c25f79d506f0d5db479afbde2b82c0156d445
Ales Saska <mail@alessaska.cz>
2019-05-28 14:27:34 -0500

    new intro page

--- jsx/utils.js
+++
@@ -1,9 +0,0 @@
-function utils() {}
-
-utils.ajax = function(url, data, action) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(data ? 'POST' : 'GET', url, true);
-    data && xhr.setRequestHeader('content-type', 'application/json');
-    action && (xhr.onreadystatechange = () => xhr.readyState === 4 && xhr.status === 200 && action(JSON.parse(xhr.responseText)));
-    xhr.send(data && JSON.stringify(data));
-}
