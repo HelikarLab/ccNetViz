d8c3f28a1048e52af032e72d1bd41065248dbd62
brickmaker <3150104891@zju.edu.cn>
2019-07-04 22:03:43 +0800

    refactor: 💡 put edgeFiller to edge.js file

--- src/layer/index.js
+++ src/layer/index.js
@@ -7,10 +7,11 @@ import ccNetViz_utils from '../utils';
 import { partitionByStyle } from '../primitiveTools';
 import ccNetViz_spatialSearch from '../spatialSearch/spatialSearch';
 import { elementShaders } from '../shaders';
-import { Line, Curve, Circle } from './shapes/edge';
+import { Line, Curve, Circle, edgesFiller } from './shapes/edge';
 import { Node, NodeColored } from './shapes/node';
 import { LineArrow, CurveArrow, CircleArrow } from './shapes/edgeArrow';
 import { Label, LabelOutline } from './shapes/labels';
+import { normalize } from './util';
 
 /**
  *  Copyright (c) 2016, Helikar Lab.
@@ -132,234 +133,19 @@ export default function(
     })(style);
   };
 
+  /*
   let normalize = (a, b) => {
     let x = b.x - a.x;
     let y = b.y - a.y;
     let sc = 1 / Math.sqrt(x * x + y * y);
     return { x: sc * x, y: sc * y };
   };
+  */
 
   let dx = Math.cos(0.9);
   let dy = Math.sin(0.9);
 
-  let ct1 = {},
-    ct2 = {},
-    ct = {};
-  let setVerticeCurveShift = (v, iV, s, t) => {
-    let csx, csy, ctx, cty, cisx, cisy, sisy, citx, city;
-    ccNetViz_geomutils.getCurveShift(t.e, ct1);
-    ctx = ct1.x;
-    cty = ct1.y;
-    citx = ct1.cx;
-    city = ct1.cy;
-
-    ccNetViz_geomutils.getCurveShift(s.e, ct2);
-    csx = ct2.x;
-    csy = ct2.y;
-    cisx = ct2.cx;
-    cisy = ct2.cy;
-
-    v.curveShift &&
-      ccNetViz_primitive.vertices(
-        v.curveShift,
-        iV,
-        -csy,
-        csx,
-        -csy,
-        csx,
-        -cty,
-        ctx,
-        -cty,
-        ctx
-      );
-    v.circleShift &&
-      ccNetViz_primitive.vertices(
-        v.circleShift,
-        iV,
-        -cisy,
-        cisx,
-        -cisy,
-        cisx,
-        -city,
-        citx,
-        -city,
-        citx
-      );
-  };
-
-  let edgesFiller = {
-    lines: style => ({
-      set: (v, e, iV, iI) => {
-        let s = ccNetViz_geomutils.edgeSource(e);
-        let t = ccNetViz_geomutils.edgeTarget(e);
-        let dx = s.x - t.x;
-        let dy = s.y - t.y;
-        let d = normalize(s, t);
-
-        setVerticeCurveShift(v, iV, s, t);
-
-        ccNetViz_primitive.vertices(
-          v.position,
-          iV,
-          s.x,
-          s.y,
-          s.x,
-          s.y,
-          t.x,
-          t.y,
-          t.x,
-          t.y
-        );
-        ccNetViz_primitive.vertices(
-          v.lengthSoFar,
-          iV,
-          0,
-          0,
-          0,
-          0,
-          dx,
-          dy,
-          dx,
-          dy
-        );
-        ccNetViz_primitive.vertices(
-          v.normal,
-          iV,
-          -d.y,
-          d.x,
-          d.y,
-          -d.x,
-          d.y,
-          -d.x,
-          -d.y,
-          d.x
-        );
-
-        if (this.hasEdgeAnimation) {
-          // when do edge animation, shader need to know the startPos and endPos
-          ccNetViz_primitive.vertices(
-            v.startPos,
-            iV,
-            s.x,
-            s.y,
-            s.x,
-            s.y,
-            s.x,
-            s.y,
-            s.x,
-            s.y
-          );
-          ccNetViz_primitive.vertices(
-            v.endPos,
-            iV,
-            t.x,
-            t.y,
-            t.x,
-            t.y,
-            t.x,
-            t.y,
-            t.x,
-            t.y
-          );
-        }
-
-        ccNetViz_primitive.quad(v.indices, iV, iI);
-      },
-    }),
-    curves: style => ({
-      numVertices: 3,
-      numIndices: 3,
-      set: (v, e, iV, iI) => {
-        let s = ccNetViz_geomutils.edgeSource(e);
-        let t = ccNetViz_geomutils.edgeTarget(e);
-        let dx = s.x - t.x;
-        let dy = s.y - t.y;
-        let d = normalize(s, t);
-
-        setVerticeCurveShift(v, iV, s, t);
-
-        ccNetViz_primitive.vertices(
-          v.position,
-          iV,
-          s.x,
-          s.y,
-          0.5 * (t.x + s.x),
-          0.5 * (t.y + s.y),
-          t.x,
-          t.y
-        );
-        ccNetViz_primitive.vertices(
-          v.lengthSoFar,
-          iV,
-          0,
-          0,
-          dx / 2,
-          dy / 2,
-          dx,
-          dy
-        );
-        ccNetViz_primitive.vertices(v.normal, iV, 0, 0, d.y, -d.x, 0, 0);
-        ccNetViz_primitive.vertices(v.curve, iV, 1, 1, 0.5, 0.0, 0, 0);
-        ccNetViz_primitive.indices(v.indices, iV, iI, 0, 1, 2);
-      },
-    }),
-    circles: style => ({
-      set: (v, e, iV, iI) => {
-        let s = ccNetViz_geomutils.edgeSource(e);
-        let d = s.y < 0.5 ? 1 : -1;
-
-        let xdiff1 = 0;
-        let ydiff1 = 0;
-        let xdiff2 = 1;
-        let ydiff2 = d;
-        let xdiff3 = 2;
-        let ydiff3 = 1.25 * d;
-        let xdiff4 = 3;
-        let ydiff4 = 1.5 * d;
-
-        setVerticeCurveShift(v, iV, s, s);
-
-        ccNetViz_primitive.vertices(
-          v.position,
-          iV,
-          s.x,
-          s.y,
-          s.x,
-          s.y,
-          s.x,
-          s.y,
-          s.x,
-          s.y
-        );
-        ccNetViz_primitive.vertices(
-          v.lengthSoFar,
-          iV,
-          xdiff1,
-          ydiff1,
-          xdiff2,
-          ydiff2,
-          xdiff3,
-          ydiff3,
-          xdiff4,
-          ydiff4
-        );
-        ccNetViz_primitive.vertices(
-          v.normal,
-          iV,
-          0,
-          0,
-          1,
-          d,
-          0,
-          1.25 * d,
-          -1,
-          d
-        );
-        ccNetViz_primitive.vertices(v.curve, iV, 1, 1, 0.5, 0, 0, 0, 0.5, 0);
-        ccNetViz_primitive.quad(v.indices, iV, iI);
-      },
-    }),
-  };
+  let ct = {};
 
   let set = (v, e, s, t, iV, iI, dx, dy) => {
     let tx = t.x;
@@ -529,11 +315,11 @@ export default function(
       if (typeof e.target == 'number') e.target = nodes[e.target];
     }
 
-    let getIndex = e => {
-      return e.uniqid || -e.index || -e.nidx;
-    };
-
     let init = () => {
+      let getIndex = e => {
+        return e.uniqid || -e.index || -e.nidx;
+      };
+
       // NOTE: here init
       for (let i = 0; i < nodes.length; i++) {
         nodes[i].index = i;
@@ -921,8 +707,8 @@ export default function(
     : {};
   let scene = (this.scene = createScene.call(this));
 
-  let loadCalled = false;
   if (!gl) {
+    let loadCalled = false;
     options.onLoad && !loadCalled && (loadCalled = true) && options.onLoad();
     return this;
   }
