8a91ae8f39ebd5c20ff105c37d7f597de27d7cc4
brickmaker <3150104891@zju.edu.cn>
2019-07-15 20:44:25 +0800

    feat: 🎸 wave shape animation basic shape done

--- src/shaders/elements/fsLineAnimateFuncs.glsl
+++ src/shaders/elements/fsLineAnimateFuncs.glsl
@@ -99,3 +99,47 @@ float isAnimateBubble() {
   // float draw = 1. - step(r, len);
   // return draw;
 }
+
+float waveCurve(float A, float B, float x) {
+  return A * (sin(PI * (x / B + 1.) * (x / B + 1.)) * (1. - step(0., x)) + -sin(PI * (-x / B + 1.) * (-x / B + 1.)) * step(0., x));
+}
+
+float isAnimateWave() {
+  vec2 pos = gl_FragCoord.xy;
+  vec2 viewport = 2. * v_screen;
+  float maxLen = length(viewport);
+
+  vec2 startPos = viewport * v_startPos;
+  vec2 endPos = viewport * v_endPos;
+
+  vec2 vec = endPos - startPos;
+  vec2 norm = normalize(vec2(-vec.y, vec.x));
+  // vec2 norm = normalize(n);
+  mat2 rotateMat = mat2(norm.y, norm.x, -norm.x, norm.y); // rotate to horizental
+
+  startPos += norm * 5.; // TODO: magic number?? don't know why I need to add this offset
+  endPos += norm * 5.;
+
+  float totalLen = distance(startPos, endPos);
+  float r = ease(fract(v_time * animateSpeed * 0.2 * maxLen / totalLen)) * totalLen;
+  // float r = 0.5 * totalLen;
+
+  float currWidth = length(dot(pos - startPos, norm));
+
+  vec2 center = startPos + normalize(vec) * r;
+  vec2 newPos = center + (rotateMat * (pos - center));
+  vec2 relativePos = newPos - center;
+
+  float shapeLen = 4. * v_animateMaxWidth; // TODO: may be configurable
+  float draw = 1.;
+  if (length(relativePos.x) < shapeLen / 2. || currWidth > v_lineWidth) {
+    draw = 0.;
+  }
+  float A = v_animateMaxWidth - v_lineWidth;
+  float B = shapeLen / 2.;
+  if (length(relativePos.x) < shapeLen / 2. && abs(waveCurve(A, B, relativePos.x) - relativePos.y) < v_lineWidth) {
+    draw = 1.;
+  }
+
+  return draw;
+}
