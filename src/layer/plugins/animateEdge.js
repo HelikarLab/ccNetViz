cdd41f84a877131e55fd3087394923959a1f32f8
brickmaker <3150104891@zju.edu.cn>
2019-08-09 13:30:31 +0800

    refactor: 💡 add arrow and cirve, etc back

--- src/layer/plugins/animateEdge.js
+++ src/layer/plugins/animateEdge.js
@@ -1,15 +1,43 @@
 import BasePlugin from './basePlugin';
 import { AnimateLine } from '../shapes/edge';
+import { Curve, Circle } from '../shapes/edge';
+import { LineArrow, CurveArrow, CircleArrow } from '../shapes/edgeArrow';
 
 export default class AnimateEdgePlugin extends BasePlugin {
   constructor(options) {
     super(options);
 
-    const { gl, edgeStyle } = options;
+    const { gl, extensions, edgeStyle, getSize, getEdgesCnt, view } = options;
 
+    // NOTE: split to different file and use getPrimitive to get webgl element
     this.scene.add('lines', new AnimateLine(gl, edgeStyle));
+
+    if (extensions.OES_standard_derivatives) {
+      this.scene.add('curves', new Curve(gl, edgeStyle));
+      this.scene.add('circles', new Circle(gl, edgeStyle));
+    }
+
+    if (edgeStyle.arrow) {
+      this.scene.add(
+        'lineArrows',
+        new LineArrow(gl, view, edgeStyle, getSize, getEdgesCnt)
+      );
+
+      if (extensions.OES_standard_derivatives) {
+        this.scene.add(
+          'curveArrows',
+          new CurveArrow(gl, view, edgeStyle, getSize, getEdgesCnt)
+        );
+        this.scene.add(
+          'circleArrows',
+          new CircleArrow(gl, view, edgeStyle, getSize, getEdgesCnt)
+        );
+      }
+    }
   }
   runRegistrations({ gl, textures }) {
+    const { extensions, edgeStyle } = this.options;
+
     let defaultAdder = (section, addSection) => {
       if (typeof section.style.texture === 'string')
         section.style.texture = textures.get(
@@ -21,5 +49,19 @@ export default class AnimateEdgePlugin extends BasePlugin {
     };
 
     this.register('lines', 'lines', defaultAdder);
+
+    if (extensions.OES_standard_derivatives) {
+      this.register('curves', 'curves', defaultAdder);
+      this.register('circles', 'circles', defaultAdder);
+    }
+
+    if (edgeStyle.arrow) {
+      this.register('lineArrows', 'lines', defaultAdder);
+
+      if (extensions.OES_standard_derivatives) {
+        this.register('curveArrows', 'curves', defaultAdder);
+        this.register('circleArrows', 'circles', defaultAdder);
+      }
+    }
   }
 }
