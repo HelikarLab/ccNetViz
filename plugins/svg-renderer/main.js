e638760e59196bd1de0acef96c6b886c5792b17f
wizArD-1910 <aman.d@ahduni.edu.in>
2020-07-13 22:02:39 +0530

    fix: updated svg api

--- plugins/svg-renderer/main.js
+++ plugins/svg-renderer/main.js
@@ -13,16 +13,42 @@ import {
 import { normalize } from '../../src/layer/util';
 
 var svg_renderer = function(svg, options) {
-  const svgHeight = baseUtils.getSVGDimensions(svg).height;
-  const svgWidth = baseUtils.getSVGDimensions(svg).width;
   let nodes = [];
   let edges = [];
   let drawEntities = {};
   let context = {};
-  context.height = parseInt(svgHeight) / 2;
-  context.width = parseInt(svgWidth) / 2;
-  context.aspect = 1;
-  context.aspect2 = 1;
+  let curveExc;
+
+  let nodeStyle = (options.styles.node = options.styles.node || {});
+  nodeStyle.minSize = nodeStyle.minSize != null ? nodeStyle.minSize : 6;
+  nodeStyle.maxSize = nodeStyle.maxSize || 16;
+  nodeStyle.color = nodeStyle.color || 'rgb(255, 255, 255)';
+
+  if (nodeStyle.label) {
+    let s = nodeStyle.label;
+    s.color = s.color || 'rgb(120, 120, 120)';
+    s.font = s.font || { type: 'Arial, Helvetica, sans-serif', size: 11 };
+  }
+
+  let edgeStyle = (options.styles.edge = options.styles.edge || {
+    arrow: {},
+  });
+  edgeStyle.width = edgeStyle.width || 1;
+  edgeStyle.color = edgeStyle.color || 'rgb(204, 04, 204)';
+  if (edgeStyle.label) {
+    let s = edgeStyle.label;
+    s.color = s.color || 'rgb(120, 120, 120)';
+    s.font = s.font || { type: 'Arial, Helvetica, sans-serif', size: 11 };
+  }
+
+  if (edgeStyle.arrow) {
+    if (typeof edgeStyle.arrow.texture !== 'undefined') {
+      let s = edgeStyle.arrow;
+      s.minSize = s.minSize != null ? s.minSize : 6;
+      s.maxSize = s.maxSize || 12;
+      s.aspect = 1;
+    }
+  }
 
   this.set = async (n, e, layout, layout_options = {}) => {
     nodes = n || [];
@@ -39,41 +65,15 @@ var svg_renderer = function(svg, options) {
       'svg_context'
     );
 
-    this.updateStyles();
-    return this;
-  };
+    const svgHeight = options.styles.svg ? options.styles.svg.height : 1000;
+    const svgWidth = options.styles.svg ? options.styles.svg.width : 1000;
+    svg.setAttribute('height', svgHeight);
+    svg.setAttribute('width', svgWidth);
 
-  this.updateStyles = () => {
-    let nodeStyle = (options.styles.node = options.styles.node || {});
-    nodeStyle.minSize = nodeStyle.minSize != null ? nodeStyle.minSize : 6;
-    nodeStyle.maxSize = nodeStyle.maxSize || 16;
-    nodeStyle.color = nodeStyle.color || 'rgb(255, 255, 255)';
-
-    if (nodeStyle.label) {
-      let s = nodeStyle.label;
-      s.color = s.color || 'rgb(120, 120, 120)';
-      s.font = s.font || { type: 'Arial, Helvetica, sans-serif', size: 11 };
-    }
-
-    let edgeStyle = (options.styles.edge = options.styles.edge || {
-      arrow: {},
-    });
-    edgeStyle.width = edgeStyle.width || 1;
-    edgeStyle.color = edgeStyle.color || 'rgb(204, 04, 204)';
-    if (edgeStyle.label) {
-      let s = edgeStyle.label;
-      s.color = s.color || 'rgb(120, 120, 120)';
-      s.font = s.font || { type: 'Arial, Helvetica, sans-serif', size: 11 };
-    }
-
-    if (edgeStyle.arrow) {
-      if (typeof edgeStyle.arrow.texture !== 'undefined') {
-        let s = edgeStyle.arrow;
-        s.minSize = s.minSize != null ? s.minSize : 6;
-        s.maxSize = s.maxSize || 12;
-        s.aspect = 1;
-      }
-    }
+    context.height = parseInt(svgHeight) / 2;
+    context.width = parseInt(svgWidth) / 2;
+    context.aspect = 1;
+    context.aspect2 = 1;
 
     let nodeSize = baseUtils.getSize(
       svg,
@@ -84,13 +84,23 @@ var svg_renderer = function(svg, options) {
     nodeStyle.size = nodeSize;
     context.nodeSize = nodeSize;
 
-    const curveExc = baseUtils.getSize(
+    curveExc = baseUtils.getSize(
       svg,
       undefined,
       baseUtils.getEdgesCnt(drawEntities),
       0.5
     );
     context.curveExc = curveExc;
+
+    return this;
+  };
+
+  this.draw = async () => {
+    svg.setAttribute(
+      'style',
+      'background-color:' + options.styles.background.color ||
+        rgb(255, 255, 255)
+    );
 
     let lines = drawEntities.lines;
     for (let i = 0; i < lines.length; i++) {
@@ -132,14 +142,6 @@ var svg_renderer = function(svg, options) {
       circle.curveExc = curveExc;
       circle.position = { x1, y1, cx1, cy1, x2, y2, cx2, cy2 };
     }
-  };
-
-  this.draw = async () => {
-    svg.setAttribute(
-      'style',
-      'background-color:' + options.styles.background.color ||
-        rgb(255, 255, 255)
-    );
 
     // hash map to store arrow head defintions
     // so as to reduce its new generation
